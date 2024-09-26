91ba1f89b6696b9b106252a565c33423304ebc53
EhoumanEvans <ehoumanevans@gmail.com>
2024-09-26 08:44:08 +0000

    first commit

---
+++ script/version_controm_process.R
@@ -0,0 +1,20 @@
+dir.create("script")
+dir.create("maintenance")
+file.create("./script/version_controm_process.R")
+
+#Tools/version/control
+
+
+# Stage all files
+
+
+# Commit stages
+
+
+# =====
+# In github
+# =====
+
+# Create a folder
+
+# copy and paste ... ord push an existing reposototy from the commande line
