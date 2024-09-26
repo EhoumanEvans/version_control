763b2375e0f3e582e039d15ae73465f5ece74028
EhoumanEvans <ehoumanevans@gmail.com>
2024-09-26 09:38:40 +0000

    changes

--- script/version_controm_process.R
+++ script/version_controm_process.R
@@ -3,6 +3,7 @@ getwd()
 dir.create("script")
 dir.create("maintenance")
 file.create("./script/version_controm_process.R")
+file.create("./script/github_process_connection.R")
 
 #Tools/version/control
 
@@ -19,5 +20,8 @@ file.create("./script/version_controm_process.R")
 
 # Create a folder
 
-# copy and paste ... ord push an existing reposototy from the commande line
+# copy and paste ... or push an existing repository from the command line
 
+
+# In Rstudio
+#Open the terminal
